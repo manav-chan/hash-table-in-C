88ca02442ea37732659fecd5fa56c4a09ea3f393
Manav Chauhan <manavchan03@gmail.com>
2024-08-13 21:05:53 +0530

    methods

--- src/map.c
+++ src/map.c
@@ -9,8 +9,9 @@ static void map_del_item(map_item*);
 void map_del(map*);
 static int map_hash(const char*, const int, const int);
 static int map_get_hash(const char*, const int, const int);
+void map_insert(map*, const char*, const char*);
 
-
+static map_item MAP_DELETED_ITEM = {NULL, NULL};
 /*
 typedef struct {
     char* key;
@@ -72,8 +73,8 @@ static void map_del_item(map_item* i) {
 static int map_hash(const char* s, const int a, const int m) {
     long hash;
     int i;
-
     const int len_s = strlen(s);
+
     hash = 0;
     for (i = 0; i < len_s; i++) {
         hash += (long)pow(a, len_s - (i+1)) * s[i];
@@ -89,4 +90,81 @@ static int map_get_hash(const char* s, const int num_buckets, const int attempt)
 
     // hash_b + 1 to avoid loop with hash_b returning 0
     return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+}
+
+// Insert a key-value pair into the map
+void map_insert(map* mp, const char* key, const char* value) {
+    map_item *item, *curr_item;
+    int index, i;
+
+    item = map_new_item(key, value);
+    index = map_get_hash(item->key, mp->size, 0);
+    curr_item = mp->items[index];
+    i = 1;
+
+    // calcualte hash index until an empty bucket is found
+    while (curr_item != NULL && curr_item != &MAP_DELETED_ITEM) {
+        // if the key already exists, update the value
+        if (curr_item != &MAP_DELETED_ITEM) {
+            if (strcmp(curr_item->key, key) == 0) {
+                map_del_item(curr_item);
+                mp->items[index] = item;
+                return;
+            }
+        }
+
+        index = map_get_hash(item->key, mp->size, i);
+        curr_item = mp->items[index];
+        i++;
+    } 
+    mp->items[index] = item;
+    mp->count++;
+}
+
+// Search for a key in the map and return the value
+char* map_search(map* mp, const char* key) {
+    int index, i;
+    map_item* item;
+
+    index = map_get_hash(key, mp->size, 0);
+    item = mp->items[index];
+    i = 1;
+    
+    // calculate hash index until the key is found
+    while (item != NULL) {
+        if (item != &MAP_DELETED_ITEM) { 
+            if (strcmp(item->key, key) == 0) {
+                return item->value;
+            }
+        }
+        index = map_get_hash(key, mp->size, i);
+        item = mp->items[index];
+        i++;
+    } 
+    return NULL;
+}
+
+// Delete a key-value pair from the map
+// using MAP_DELETED_ITEM as separate chaining may be used to maintain linked list.
+void map_delete(map* mp, const char* key) {
+    int index, i;
+    map_item* item;
+
+    index = map_get_hash(key, mp->size, 0);
+    item = mp->items[index];
+    i = 1;
+    
+    // calculate hash index until the key is found
+    while (item != NULL) {
+        if (item != &MAP_DELETED_ITEM) {
+            if (strcmp(item->key, key) == 0) {
+                map_del_item(item);
+                mp->items[index] = &MAP_DELETED_ITEM;
+            }
+        }
+        index = map_get_hash(key, mp->size, i);
+        item = mp->items[index];
+        i++;
+    } 
+    mp->count--;
 }
